8876c91df3b4e5468bd87e38fba5417e236c2c21
Vitor Campos <vito.r.natal@hotmail.com>
2021-05-30 14:57:52 -0300

    added images, API and extension

---
+++ challenge-ios/Model/ApiReturn.swift
@@ -0,0 +1,24 @@
+//
+//  ApiReturn.swift
+//  challenge-ios
+//
+//  Created by Thiago Pontes Lima on 28/05/21.
+//  Copyright © 2021 Vitor Campos. All rights reserved.
+//
+
+import Foundation
+//MARK: - ReturnApi
+//struct para moldar os dados vindos da api
+
+struct ReturnApiPizza: Codable {
+    let id, name, imageUrl: String
+    let rating: Int
+    let priceP, priceM, priceG: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, imageUrl
+        case rating
+        case priceP, priceM, priceG
+    }
+}
+
---
+++ challenge-ios/ViewModel/ApiConnection.swift
@@ -0,0 +1,49 @@
+//
+//  ApiConnection.swift
+//  challenge-ios
+//
+//  Created by Thiago Pontes Lima on 28/05/21.
+//  Copyright © 2021 Vitor Campos. All rights reserved.
+
+import Alamofire
+import Foundation
+import RxSwift
+
+class ApiConnection{
+    
+    let loginUrl = "https://p3teufi0k9.execute-api.us-east-1.amazonaws.com/v1/signin"
+    let pizzaUrl = "https://p3teufi0k9.execute-api.us-east-1.amazonaws.com/v1/pizza"
+    var credential = ["email": "user@xds.com.br", "password": "223344"]
+    
+    //MARK: - Login
+    public func login(){
+        let params = createLoginParams(user: "", pass: "")
+        _ = AF.request(loginUrl, method: .post,parameters: params).responseJSON{
+            data in
+            print(data)
+        }
+    }
+    
+    //MARK: - PizzaReturn
+    public func fetchPizzas() -> Observable<[ReturnApiPizza]>{
+        return Observable.create{ observer -> Disposable in
+            let task = AF.request(self.pizzaUrl).responseJSON(completionHandler: {(data) in
+                do {
+                    let pizzas = try JSONDecoder().decode([ReturnApiPizza].self, from: data.data!)
+                    observer.onNext(pizzas)
+                } catch {
+                    observer.onError(error)
+                }
+            })
+            task.resume()
+            return Disposables.create {
+            task.cancel()
+            }
+        }
+    }
+    
+    //MARK: - CreatingLoginParams
+    private func createLoginParams(user: String, pass: String) -> [String : String] {
+        return ["email": user, "password": pass]
+    }
+}
